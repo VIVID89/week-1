8a04508577c0ab231248f37e425d5a947160f508
VIVID89 <wskth0930@gmail.com>
2023-02-03 16:18:51 +0900

    컴포넌트 수업

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <---- 자바스크립트 영역 ---->
+    // function onClickButtonHandler () {
+    //   return alert("클릭!");
+    // }
+    const onClickButtonHandler = () => alert("클릭!");
+      
+    
+
+    return (
+        /* <---- HTML/JSX 영역  ---->*/
+        <div
+            style={{   //jsx style 속성에 객체 형식으로 집어넣는다. 
+                height: '100vh',  
+                display: ' flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <span>이것은 내가 만든 App컴포넌트 입니다.</span>
+            <button onClick={onClickButtonHandler}>클릭!</button>
+        </div>
+    );
 }
 
 export default App;
