dd76adb61a726c6cfd67090100c1abba8d17118e
VIVID89 <wskth0930@gmail.com>
2023-02-04 00:10:11 +0900

    props children

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-
-//props를 통해 부모 -> 자식 데이터가 전달됐다.
-function Son(props) {
-    console.log('props', props.gfName);
-    return <div>나는 {props.gfName}의 손자입니다!</div>;
-}
-//부모 -> 자식 정보를 전달했다.
-function Mother(props) {
-  console.log(props)
-    const gfName = props.grandFatherName;
-    const name = '흥부인';
-    return <Son gfName={gfName} />;
-}
-
-function GrandFather() {
-    const name = '흥부할배';
-    return <Mother grandFatherName={name} />;
-}
+import Layout from 'Layout';
 
 function App() {
-    return <GrandFather />;
+  return <Layout>
+    <div>App 컴포넌트에서 보낸 값입니다!</div>
+  </Layout>
 }
 
-export default App;
+export default App
