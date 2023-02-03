0975b47ffa27ef93aefcfe41b3c2d7251f7d0d75
VIVID89 <wskth0930@gmail.com>
2023-02-03 22:52:24 +0900

    JSX의 기초

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-function Son() {
-  return <div>나는 아들입니다.</div>;
+export default function App() {
+  const number = 11; 
+
+  const pTagStyle = {
+    color: "red",
+  };
+  return (
+    <div>
+      <p style ={pTagStyle} >안녕하세요 리액트입니다.</p>
+
+      <p style ={pTagStyle} >
+      { number > 10 ? number + "은 10보다 크다." : number + "은 10보다 작다."}
+      </p>
+        
+      </div>
+      
+  );
 }
-
-function Mother() {
-  return <Son />;
-}
-
-function GrandFather() {
-  return <Mother />;
-}
-
-function App() {
-  return <GrandFather />;    
-}
-
-export default App;
