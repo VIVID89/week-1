258fbce98d1f52aff6136f8b635f00755555eb6e
VIVID89 <wskth0930@gmail.com>
2023-02-03 23:54:13 +0900

    props 드릴링

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-export default function App() {
-  const number = 11; 
-
-  const pTagStyle = {
-    color: "red",
-  };
-  return (
-    <div>
-      <p style ={pTagStyle} >안녕하세요 리액트입니다.</p>
-
-      <p style ={pTagStyle} >
-      { number > 10 ? number + "은 10보다 크다." : number + "은 10보다 작다."}
-      </p>
-        
-      </div>
-      
-  );
+//props를 통해 부모 -> 자식 데이터가 전달됐다.
+function Son(props) {
+    console.log('props', props.gfName);
+    return <div>나는 {props.gfName}의 손자입니다!</div>;
 }
+//부모 -> 자식 정보를 전달했다.
+function Mother(props) {
+  console.log(props)
+    const gfName = props.grandFatherName;
+    const name = '흥부인';
+    return <Son gfName={gfName} />;
+}
+
+function GrandFather() {
+    const name = '흥부할배';
+    return <Mother grandFatherName={name} />;
+}
+
+function App() {
+    return <GrandFather />;
+}
+
+export default App;
