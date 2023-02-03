4d0459a9374124505a9e50cfeef2a0eb1908a3fd
VIVID89 <wskth0930@gmail.com>
2023-02-03 16:54:17 +0900

    할아버지,엄마,아들 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-//자녀 컴포넌트
-function Child() { //컴포넌트 이름은 대문자로 시작한다.
+function Son() {
   return <div>나는 아들입니다.</div>;
 }
 
-// 부모 컴포넌트
+function Mother() {
+  return <Son />;
+}
+
+function GrandFather() {
+  return <Mother />;
+}
+
 function App() {
-  return <Child />;    
+  return <GrandFather />;    
 }
 
 export default App;
