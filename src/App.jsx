a3a20656fd30e9a7440eab36e2bde5eb4c40f8d1
VIVID89 <wskth0930@gmail.com>
2023-02-04 09:55:03 +0900

    props 추출, 구조분해 할당, props default

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react'
+import Child from 'Child';
+
+
+function App() {
+  const name ="test";
+
+  return (
+    <Child age={21} name={name}>
+    이름
+    </Child>
+  ) 
+}
+
+export default App
