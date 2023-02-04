bc4586bc70ba4e3b7704afe2092068b74946be77
VIVID89 <wskth0930@gmail.com>
2023-02-04 10:53:37 +0900

    useState 예제

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,43 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-    const [fruit, setFruit] = useState('');
+    const [id, setId] = useState('');
+    const [pw, setPw] = useState('');
+
+    const onIdChangeHandler = (event) => {
+        setId(event.target.value);
+    };
+    const onPwChangeHandler = (event) => {
+        setPw(event.target.value);
+    };
 
     return (
         <div>
-            과일 :{' '}
-            <input
-                value={fruit}
-                onChange={function (event) {
-                    // console.log(event.target.value);
-                    setFruit(event.target.value);
-                }}
+            <div>
+            아이디: <input type = "text"
+                value={id}
+                onChange={onIdChangeHandler}
             />
+            </div>
+            <div>
+            비밀번호 : <input type="password" 
+                value={pw}
+                onChange={onPwChangeHandler}
+            /> 
+            </div>
+            <div>
+                <button onClick={()=>{
+                    alert(`고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`
+                    );
+                    setId("");
+                    setPw("");
+                }}
+                >
+                로그인
+                </button>
+            </div>
+            
+            
         </div>
     );
 }
